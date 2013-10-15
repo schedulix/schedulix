b933f1001c04095e653b7f8cabb9cf2bba6bef75
Ronald Jeninga <ronald.jeninga@independit.de>
2013-10-15 14:54:03 +0200

    Faster communication in the python protocol by eliminating intermediate objects

--- src/server/output/SDMSPythonRenderer.java
+++ src/server/output/SDMSPythonRenderer.java
@@ -40,6 +40,8 @@ public class SDMSPythonRenderer extends SDMSScriptRenderer
 
 	public final static String __version = "@(#) $Id: SDMSPythonRenderer.java,v 2.2.18.1 2013/03/14 10:24:18 ronald Exp $";
 
+	public static volatile boolean directIO = true;
+
 	public SDMSPythonRenderer ()
 	{
 		super();
@@ -47,7 +49,10 @@ public class SDMSPythonRenderer extends SDMSScriptRenderer
 
 	public void render(SystemEnvironment env, SDMSOutput p_output) throws FatalException
 	{
-		render(env.cEnv.ostream(), p_output);
+		if (directIO)
+			renderStream(env.cEnv.ostream(), p_output);
+		else
+			render(env.cEnv.ostream(), p_output);
 	}
 
 	public void render(PrintStream ostream, SDMSOutput p_output) throws FatalException
@@ -76,6 +81,32 @@ public class SDMSPythonRenderer extends SDMSScriptRenderer
 		sb.append("}\n");
 
 		ostream.print(sb);
+
+	}
+
+	public void renderStream(PrintStream ostream, SDMSOutput p_output) throws FatalException
+	{
+		ostream.print("{\n");
+		boolean first = true;
+		if (p_output.container != null) {
+			ostream.print("'DATA' : \n");
+			renderContainer(ostream, p_output.container);
+			first = false;
+		}
+		if (p_output.feedback != null) {
+			if (!first) {
+				ostream.print( ",");
+			}
+			first = false;
+			renderFeedback(ostream, p_output.feedback);
+		}
+		if (p_output.error != null) {
+			if (!first) {
+				ostream.print( ",");
+			}
+			renderError(ostream, p_output.error);
+		}
+		ostream.print("}\n");
 
 	}
 
@@ -187,6 +218,117 @@ public class SDMSPythonRenderer extends SDMSScriptRenderer
 	}
 
 	private	void renderPrompt(StringBuffer sb)
+	{
+	}
+
+	private void renderContainer(PrintStream sb, SDMSOutputContainer p_container)
+	{
+		sb.print("{\n");
+		if (p_container.title != null) {
+			sb.print("'TITLE' : '" +  maskQuotes(p_container.title.toString()) + "',\n");
+		}
+		sb.print("'DESC' : [\n");
+		boolean first = true;
+		Iterator i = p_container.desc.iterator();
+		while (i.hasNext()) {
+			if (!first) {
+				sb.print(",\n");
+			}
+			first = false;
+			SDMSOutputLabel l = (SDMSOutputLabel)i.next();
+			sb.print("'" + l.name + "'\n");
+		}
+		sb.print("],\n");
+		if (p_container.columns == 0) {
+			sb.print("'RECORD' : ");
+			renderRecord(sb, p_container);
+		} else {
+			sb.print("'TABLE' : ");
+			renderTable(sb, p_container);
+		}
+		sb.print("}\n");
+	}
+
+	private void renderRecord(PrintStream sb, SDMSOutputContainer p_container)
+	{
+		int i;
+		int s = p_container.desc.size();
+		String name;
+		boolean first = true;
+
+		Vector v = (Vector)(p_container.dataset.elementAt(0));
+
+		sb.print("{\n");
+		for (i = 0; i < s; i++) {
+			if (!first) {
+				sb.print(",\n");
+			}
+			first = false;
+			name = ((SDMSOutputLabel)(p_container.desc.elementAt(i))).name;
+			sb.print("'" + name  + "' : ");
+			if(v.elementAt(i) != null) {
+				if (!(v.elementAt(i) instanceof SDMSOutputContainer)) {
+					sb.print("'" + maskQuotes((v.elementAt(i)).toString()) + "'");
+				} else {
+					sb.print("\n");
+					renderContainer (sb, (SDMSOutputContainer)(v.elementAt(i)));
+				}
+			} else {
+				sb.print("None");
+			}
+		}
+		sb.print("}\n");
+	}
+
+	private void renderTable(PrintStream sb, SDMSOutputContainer p_container)
+	{
+		int i;
+		int s = p_container.desc.size();
+		String name;
+		boolean first = true;
+
+		sb.print( "[\n");
+		Iterator it = p_container.dataset.iterator();
+		while(it.hasNext()) {
+			if (!first) {
+				sb.print(",\n");
+			}
+			first = false;
+			sb.print( "{\n");
+			Vector v = (Vector)(it.next());
+			boolean rfirst = true;
+			for (i = 0; i < s; i++) {
+				if (!rfirst) {
+					sb.print(",\n");
+				}
+				rfirst = false;
+				name = ((SDMSOutputLabel)(p_container.desc.elementAt(i))).name;
+				sb.append("'" + name + "' : ");
+				if(v.elementAt(i) == null) {
+					sb.print("None");
+				} else {
+					sb.print("'" + maskQuotes((v.elementAt(i)).toString()) + "'");
+				}
+			}
+			sb.print("}\n");
+		}
+		sb.print("]\n");
+	}
+
+	private void renderFeedback(PrintStream sb, Object p_feedback)
+	{
+		sb.print("'FEEDBACK' : '" +  maskQuotes(p_feedback.toString()) + "'\n");
+	}
+
+	private	void renderError(PrintStream sb, SDMSOutputError p_error)
+	{
+		sb.print("'ERROR' : {\n");
+		sb.print("'ERRORCODE' : '" + maskQuotes(p_error.code.toString()) + "', \n");
+		sb.print("'ERRORMESSAGE' : '" + maskQuotes(p_error.message.toString()) + "'\n");
+		sb.print("}\n");
+	}
+
+	private	void renderPrompt(PrintStream sb)
 	{
 	}
 
