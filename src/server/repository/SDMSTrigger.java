9d166693c3e76eb83d1ebde431ddabdb3208f940
Ronald Jeninga <ronald.jeninga@independit.de>
2018-12-04 16:48:27 +0100

    allow administrators to drop orphaned triggers

--- src/server/repository/SDMSTrigger.java
+++ src/server/repository/SDMSTrigger.java
@@ -576,6 +576,7 @@ public class SDMSTrigger extends SDMSTriggerProxyGeneric
 		p = checkPrivs;
 		int objectType = getObjectType(env).intValue();
 		SDMSTable t;
+		boolean found = false;
 
 		switch (objectType) {
 			case JOB_DEFINITION:
@@ -588,6 +589,7 @@ public class SDMSTrigger extends SDMSTriggerProxyGeneric
 						sp |= SDMSPrivilege.CREATE | SDMSPrivilege.DROP | SDMSPrivilege.VIEW;
 					}
 					p = p & sp;
+					found = true;
 				} catch (NotFoundException nfe) {
 					p = 0;
 				}
@@ -601,6 +603,7 @@ public class SDMSTrigger extends SDMSTriggerProxyGeneric
 						sp |= SDMSPrivilege.CREATE | SDMSPrivilege.DROP | SDMSPrivilege.VIEW;
 					}
 					p = p & sp;
+					found = true;
 				} catch (NotFoundException nfe) {
 					p = 0;
 				}
@@ -614,10 +617,20 @@ public class SDMSTrigger extends SDMSTriggerProxyGeneric
 						sp |= SDMSPrivilege.CREATE | SDMSPrivilege.DROP | SDMSPrivilege.VIEW;
 					}
 					p = p & sp;
+					found = true;
 				} catch (NotFoundException nfe) {
 					p = 0;
 				}
 				break;
+		}
+		if (!found) {
+			if(env.tx.mode == SDMSTransaction.READONLY) {
+				throw new CommonErrorException (new SDMSMessage(env, "03809050935", "Orphan Trigger found, as ADMIN use 'drop trigger $1' to solve this problem", getId(env).toString()));
+			} else {
+				if (env.cEnv.gid().contains(SDMSObject.adminGId)) {
+					p = checkPrivs;
+				}
+			}
 		}
 		return p;
 	}
