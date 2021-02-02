5234a2616683905798ebb4541adfbea292752de8
Ronald Jeninga <ronald.jeninga@independit.de>
2021-02-02 12:52:44 +0100

    schema changes for user parameters

---
+++ sql/pg_gen/USER_PARAMETER.sql
@@ -0,0 +1,53 @@
+/*
+Copyright (c) 2000-2013 "independIT Integrative Technologies GmbH",
+Authors: Ronald Jeninga, Dieter Stubler
+
+schedulix Enterprise Job Scheduling System
+
+independIT Integrative Technologies GmbH [http://www.independit.de]
+mailto:contact@independit.de
+
+This file is part of schedulix
+
+schedulix is free software: 
+you can redistribute it and/or modify it under the terms of the 
+GNU Affero General Public License as published by the 
+Free Software Foundation, either version 3 of the License, 
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+-- Copyright (C) 2001,2002 topIT Informationstechnologie GmbH
+-- Copyright (C) 2003-2014 independIT Integrative Technologies GmbH
+
+CREATE TABLE USER_PARAMETER (
+    ID                             decimal(20) NOT NULL
+    , U_ID                           decimal(20)     NOT NULL
+    , NAME                           varchar(64)     NOT NULL
+    , VALUE                          varchar(256)        NULL
+    , IS_LONG                        integer         NOT NULL
+    , CREATOR_U_ID                   decimal(20)     NOT NULL
+    , CREATE_TS                      decimal(20)     NOT NULL
+    , CHANGER_U_ID                   decimal(20)     NOT NULL
+    , CHANGE_TS                      decimal(20)     NOT NULL
+);
+CREATE UNIQUE INDEX PK_USER_PARAMETER
+ON USER_PARAMETER(ID);
+CREATE VIEW SCI_USER_PARAMETER AS
+SELECT
+    ID
+    , U_ID                           AS U_ID
+    , NAME                           AS NAME
+    , VALUE                          AS VALUE
+    , CASE IS_LONG WHEN 1 THEN 'TRUE' WHEN 0 THEN 'FALSE' END AS IS_LONG
+    , CREATOR_U_ID                   AS CREATOR_U_ID
+    , timestamptz 'epoch' + cast(to_char(mod(CREATE_TS, 1125899906842624)/1000, '999999999999') as interval) AS CREATE_TS
+    , CHANGER_U_ID                   AS CHANGER_U_ID
+    , timestamptz 'epoch' + cast(to_char(mod(CHANGE_TS, 1125899906842624)/1000, '999999999999') as interval) AS CHANGE_TS
+  FROM USER_PARAMETER;
