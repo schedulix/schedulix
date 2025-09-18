7c7fcc7a21f0cd960d3f43c6ad5d31eaba18170e
Ronald Jeninga <ronald.jeninga@independit.de>
2025-09-18 16:35:54 +0200

    With TestConfig the configuration file and database connectivity can be tested

--- src/TestConfig.java
+++ src/TestConfig.java
@@ -1,7 +1,28 @@
-//
-// TestConfig
-//
-// Copyright (C) 2015 independIT Integrative Technologies GmbH
+/*
+Copyright (c) 2000-2013 "independIT Integrative Technologies GmbH",
+Authors: Ronald Jeninga, Dieter Stubler
+
+schedulix Enterprise Job Scheduling System
+
+independIT Integrative Technologies GmbH [http://www.independit.de]
+mailto:contact@independit.de
+
+This file is part of schedulix
+
+schedulix is free software:
+you can redistribute it and/or modify it under the terms of the
+GNU Affero General Public License as published by the
+Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package de.independit.scheduler;
 
@@ -151,14 +172,13 @@ public class TestConfig
 			String k = (String) e.nextElement();
 			if(k.equals(SystemEnvironment.S_DBPASSWD))		continue;	// for security reasons we skip the password
 			if(k.equals(SystemEnvironment.S_SYSPASSWD))		continue;	// for security reasons we skip the password
-//<OS-DISABLED-EXCLUDE> FEATURE_SSL
 			if(k.equals(SystemEnvironment.S_KEYSTOREPASSWORD))	continue;	// for security reasons we skip the password
 			if(k.equals(SystemEnvironment.S_TRUSTSTOREPASSWORD))	continue;	// for security reasons we skip the password
-//</OS-EXCLUDE>
 			SDMSThread.doTrace(null, k + "=" + props.getProperty(k), SDMSThread.SEVERITY_INFO);
 		}
 		      
 		env = new SystemEnvironment(props, programLevel, buildDate, buildHash, null);
+		Server.setIniFile(inifile);
 
 		try {
 			Connection c = Server.connectToDB(env);
@@ -173,7 +193,7 @@ public class TestConfig
 			} catch (Exception e) {
 				// ignore
 			}
-		} catch (SDMSException e) {	// CHECKED
+		} catch (SDMSException e) {
 			System.out.println("Connection failed ... :-(");
 			System.out.println(e.toString());
 		}
